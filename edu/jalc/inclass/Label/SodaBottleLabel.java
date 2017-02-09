2f925eae3126e143b5bef5e2da29f6352967bde3
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-02-09 08:49:53 -0600

    SodaBottleLabel Updated

--- edu/jalc/inclass/Label/SodaBottleLabel.java
+++ edu/jalc/inclass/Label/SodaBottleLabel.java
@@ -2,8 +2,8 @@ package edu.jalc.inclass.label;
 public class SodaBottleLabel{
 
   private String brand;
-  private Logo logo;
-  private Ingredient ingredient;
+  private final Logo logo;
+  private final Ingredient ingredient;
 
   public SodaBottleLabel(String brand, Logo logo, Ingredient ingredient){
     this.brand = brand;
